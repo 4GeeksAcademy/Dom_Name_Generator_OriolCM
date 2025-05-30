fefb8cd638f4db349f92ea2a2a4849dc9e16c9c9
Oriol383 <oriolcm02@gmail.com>
2025-05-30 11:00:56 +0000

    firstcommit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ['the', 'our'];
+let adj = ['green', 'prancing'];
+let noun = ['dragon', 'pony'];
+let domain = ['.com', '.es'];
+
+let pronombre = Math.floor(Math.random() * pronoun.length);
+let adjetivo = Math.floor(Math.random() * adj.length);
+let nombre = Math.floor(Math.random() * noun.length);
+let dominio = Math.floor(Math.random() * domain.length);
+
+document.querySelector("#array").innerHTML = pronoun[pronombre] + adj[adjetivo] + noun[nombre] + domain[dominio];
 };
+

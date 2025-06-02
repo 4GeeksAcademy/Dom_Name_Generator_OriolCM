83c4bcad837daf4ba6b2b4f42346ef4ddce31740
Oriol383 <oriolcm02@gmail.com>
2025-06-02 09:07:39 +0000

    firtscommit

--- src/app.js
+++ src/app.js
@@ -18,5 +18,16 @@ let nombre = Math.floor(Math.random() * noun.length);
 let dominio = Math.floor(Math.random() * domain.length);
 
 document.querySelector("#array").innerHTML = pronoun[pronombre] + adj[adjetivo] + noun[nombre] + domain[dominio];
+
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
+
 };
 
